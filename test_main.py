a7fd553c2c7648c2f4f4c478878163534b538ce9
Nicolas Depelteau <71102008+Depdx@users.noreply.github.com>
2022-04-04 10:10:08 -0400

    ND:

--- test_main.py
+++ test_main.py
@@ -1,25 +1,52 @@
 import json
+from random import random
+
 from main import evaluate
 from main import DEFAULT_TRAINING_SET
 import unittest
+from text_cleaner import TextCleaning
 
 
 class TestMain(unittest.TestCase):
 	def setUp(self):
 		self.baseline = evaluate(test_set_json_path=DEFAULT_TRAINING_SET)
+		self.cleaner = TextCleaning()
 
 	def f1_comparison(self, json_path: str):
 		score_f1 = evaluate(test_set_json_path=json_path)
-		self.assertAlmostEqual(self.baseline, score_f1, 2, "F1-score is not equivalent", 0.03)
+		self.assertAlmostEqual(self.baseline, score_f1, delta=0.03)
 
 	def test_nettoyage_train(self):
-		pass
+		path = "json-metamorphique/train_clean.json"
+		with open("train_set.json") as file:
+			dataset = json.load(file)
+			for row in dataset["dataset"]:
+				row["mail"]["Subject"] = " ".join(self.cleaner.clean_text(row["mail"]["Subject"]))
+				row["mail"]["Body"] = " ".join(self.cleaner.clean_text(row["mail"]["Body"]))
+			out_file = open(path, "w")
+			json.dump(dataset, out_file, indent=6)
+			out_file.close()
+		self.f1_comparison(path)
 
 	def test_nettoyage_test(self):
-		pass
+		path = "json-metamorphique/test_clean.json"
+		with open("test_set.json") as file:
+			dataset = json.load(file)
+			for row in dataset["dataset"]:
+				row["mail"]["Subject"] = " ".join(self.cleaner.clean_text(row["mail"]["Subject"]))
+				row["mail"]["Body"] = " ".join(self.cleaner.clean_text(row["mail"]["Body"]))
+			out_file = open(path, "w")
+			json.dump(dataset, out_file, indent=6)
+			out_file.close()
+		self.f1_comparison(path)
 
 	def test_permutation_train(self):
-		pass
+		with json.load("train_set.json") as file:
+			dataset = file["dataset"]
+			for row in dataset:
+				tokens = row["body"].split()
+				for _ in range(10):
+					random.randint(0, len(tokens))
 
 	def test_permutation_test(self):
 		pass
@@ -35,4 +62,3 @@ class TestMain(unittest.TestCase):
 
 	def test_duplicate_test(self):
 		pass
-
